af351d4677a2310c9e41cacaf091d554aed71e8f
MasterLearner101 <145219876+MasterLearner101@users.noreply.github.com>
2023-12-21 13:25:57 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "us-east-1"
 
 }
-resource "aws_instance" "app-dev" {
+resource "aws_instance" "app_dev" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
 
